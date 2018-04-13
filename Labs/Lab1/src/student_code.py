38bf64c553a8320244e8dcbf2d842336a676a1be
Alex Grimes <alexkgrimes@gmail.com>
2018-04-12 20:23:44 -0500

    done: breadth first search

--- Labs/Lab1/src/student_code.py
+++ Labs/Lab1/src/student_code.py
@@ -24,49 +24,43 @@ def bf_search(map):
 	queue = []
 	predecessors = {}
 
-	# mark the start node as visited and add to queue, path
+	# add to queue, path
 	yStart, xStart = start
-	map[yStart][xStart] = 5
 	queue.insert(0, (yStart, xStart))
 	predecessors[start] = None
 
 	while (len(queue) > 0):
 		y, x = queue.pop() 
-		print "indices = ", y, x, "value = ", map[y][x]
 
+		# Found the end!
 		if map[y][x] == 3:
 			map[y][x] == 5
-			point = (y, x)
-			print "before constructPath"
-			constructPath(predecessors, point, map)
+			constructPath(predecessors, (y, x), map)
+			map[yStart][xStart] = 5
 			return True
 
+		# Mark as visited and process children
+		map[y][x] = 4
 		if inRangeAndNotVisited(y, x + 1, map):
-			if map[y][x + 1] != 3:
-				map[y][x + 1] = 4
 			queue.insert(0, (y, x + 1))
 			predecessors[(y, x + 1)] = (y, x)
 		if inRangeAndNotVisited(y + 1, x, map):
-			if map[y + 1][x] != 3:
-				map[y + 1][x] = 4
 			queue.insert(0, (y + 1, x))
 			predecessors[(y + 1, x)] = (y, x)
 		if inRangeAndNotVisited(y, x - 1, map):
-			if map[y][x - 1] != 3:
-				map[y][x - 1] = 4
 			queue.insert(0, (y, x - 1))
 			predecessors[(y, x - 1)] = (y, x) 
 		if inRangeAndNotVisited(y - 1, x, map):
-			if map[y - 1][x] != 3:
-				map[y - 1][x] = 4
 			queue.insert(0, (y - 1, x))
 			predecessors[(y - 1, x)] = (y, x) 
 
 	return False
 
+# Check for in bounds and not already visited
 def inRangeAndNotVisited(y, x, map):
 	return (y >= 0 and y < common.constants.MAP_HEIGHT and x >= 0 and x < common.constants.MAP_WIDTH and (map[y][x] == 0 or map[y][x] == 3))
 
+# Draw the path from the end to start
 def constructPath(predecessors, currPoint, map):
 	print "constructPath"
 	print "end point: ", currPoint
@@ -84,10 +78,5 @@ def constructPath(predecessors, currPoint, map):
 # stack = []
 # stack.append()
 # stack.pop()
-
-# breadth first
-# queue = []
-# queue.insert(0, item)
-# queue.pop()
 
 # First [y][x+1], then [y+1][x], then [y][x-1], and finally [y-1][x]
