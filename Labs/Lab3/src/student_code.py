ce888bb0f1831342b18186a0903ad786b43638a5
Alex Grimes <alexkgrimes@gmail.com>
2018-04-25 21:52:43 -0500

    Done: ab pruning, all test cases pass, clean up

--- Labs/Lab3/src/student_code.py
+++ Labs/Lab3/src/student_code.py
@@ -1,11 +1,7 @@
 import common
 
+# --------------- NORMAL MINMAX ------------------- #
 def minmax_tictactoe(board, turn):
-	#put your code here:
-	#it must return common.constants.X(1), common.constants.O(2) or common.constants.NONE(0) for tie.
-	#use the function common.game_status(board), to evaluate a board
-	#it returns common.constants.X(1) if X wins, common.constants.O(2) if O wins or common.constants.NONE(0) if tie or game is not finished
-	#the program will keep track of the number of boards evaluated
 	
 	result = common.game_status(board);
 
@@ -44,14 +40,65 @@ def maxX(board):
 			board[i] = common.constants.NONE
 	return v
 
-# helper to test for ties vs. not done
-def boardFull(board):
-	for space in board:
-		if space == common.constants.NONE:
-			return False
-	return True
+# -------------------- MINMAX WITH AB PRUNING ----------------------# 
+def abprun_tictactoe(board, turn):
+	# just return the helper because need to pass alpha and beta around
+	return abprun_helper(board, turn, -1, -1)
 
+def abprun_helper(board, turn, alpha, beta):
+	result = common.game_status(board);
+
+	# alpha is worst so far for O
+	alpha = common.constants.X
+	# beta is worst so far for X
+	beta = common.constants.O
+
+	# terminal state, someone winning
+	if result == common.constants.X or result == common.constants.O:
+		return result
+	# terminal state, tie
+	if result == common.constants.NONE and boardFull(board):
+		return result
+
+	# otherwise, keep playing
+	if turn == common.constants.X:
+		return maxAbX(board, alpha, beta)
+	if turn == common.constants.O:
+		return maxAbO(board, alpha, beta)
+
+def maxAbO(board, alpha, beta):
+	# set not to -inf, because X is worst you can do
+	v = common.constants.X
+	for i, space in enumerate(board):
+		# if the space is empty, you can put an O there
+		if space == common.constants.NONE:
+			board[i] = common.constants.O
+			v = max_o(v, abprun_helper(board, common.constants.X, alpha, beta))
+			board[i] = common.constants.NONE
+			# if v is better than beta, return (prune)
+			if v == max_o(v, beta):
+				return v
+			alpha = max_o(alpha, v)
+	return v
+
+def maxAbX(board, alpha, beta):
+	# set not to inf, because O is worst you can do
+	v = common.constants.O
+	for i, space in enumerate(board):
+		# if the space is empty, you can put an X there
+		if space == common.constants.NONE:
+			board[i] = common.constants.X
+			v = max_x(v, abprun_helper(board, common.constants.O, alpha, beta))
+			board[i] = common.constants.NONE
+			# if v is better than alpha, return (prune)
+			if v == max_x(v, alpha):
+				return v
+			beta = max_x(beta, v)
+	return v
+
+# ------------------ communal helpers ---------------------#
 # both named max because they are both just trying to max themselves winning
+# use for both normal minmax and with ab pruning
 def max_x(v, minmax):
 	if v == common.constants.X or minmax == common.constants.X:
 		return common.constants.X
@@ -66,12 +113,9 @@ def max_o(v, minmax):
 		return common.constants.NONE
 	return common.constants.X
 
-
-def abprun_tictactoe(board, turn):
-	#put your code here:
-	#it must return common.constants.X(1), common.constants.O(2) or common.constants.NONE(0) for tie.
-	#use the function common.game_status(board), to evaluate a board
-	#it returns common.constants.X(1) if X wins, common.constants.O(2) if O wins or common.constants.NONE(0) if tie or game is not finished
-	#the program will keep track of the number of boards evaluated
-	#result = common.game_status(board);
-	return common.constants.NONE
+# helper to test for ties vs. not done
+def boardFull(board):
+	for space in board:
+		if space == common.constants.NONE:
+			return False
+	return True
